69ca24a63e12a3e14caf2731f68e3748f800ff3e
Timóteo Dima Pereira <timoteo5@icloud.com>
2024-11-06 16:21:48 +0000

    Solicitação dos dados

--- exercicios/banco-terminal/Conta-banco/src/ContaTerminal.java
+++ exercicios/banco-terminal/Conta-banco/src/ContaTerminal.java
@@ -9,7 +9,23 @@ public class ContaTerminal {
         String agencia, nomeCliente;
         double saldo;
 
-     
+        // Solicitação dos dados ao usuário
+        System.out.print("Por favor, digite o número da agência: ");
+        agencia = scanner.nextLine();
+
+        System.out.print("Por favor, digite o número da conta: ");
+        numero = scanner.nextInt();
+
+
+        scanner.nextLine(); // Consumir a quebra de linha pendente
+
+        System.out.print("Por favor, digite o nome do cliente: ");
+        nomeCliente = scanner.nextLine();
+
+        System.out.print("Por favor, digite o saldo:");
+        saldo = scanner.nextDouble();
+
+        
 
         scanner.close(); // Fecha o Scanner para liberar recursos
     }
