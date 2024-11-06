4685b8a345eaafd1f7a3dfa45225cff6e3ca7856
Timóteo Dima Pereira <timoteo5@icloud.com>
2024-11-06 16:18:21 +0000

    Criação da classe Scanner, Declarção dos variáveis

--- exercicios/banco-terminal/Conta-banco/src/ContaTerminal.java
+++ exercicios/banco-terminal/Conta-banco/src/ContaTerminal.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Declaração das variáveis com os tipos corretos
+        int numero;
+        String agencia, nomeCliente;
+        double saldo;
+
+     
+
+        scanner.close(); // Fecha o Scanner para liberar recursos
     }
 }
