a024ea87cd5fd46fbe84475872cc6d734e39818a
Timóteo Dima Pereira <timoteo5@icloud.com>
2024-11-06 16:22:17 +0000

    Exibição do resultado esperado

--- exercicios/banco-terminal/Conta-banco/src/ContaTerminal.java
+++ exercicios/banco-terminal/Conta-banco/src/ContaTerminal.java
@@ -25,7 +25,9 @@ public class ContaTerminal {
         System.out.print("Por favor, digite o saldo:");
         saldo = scanner.nextDouble();
 
-        
+        // Exibição da mensagem de boas-vindas
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+                ", conta " + numero + " e seu saldo " + saldo + " já está  disponível para saque.");
 
         scanner.close(); // Fecha o Scanner para liberar recursos
     }
